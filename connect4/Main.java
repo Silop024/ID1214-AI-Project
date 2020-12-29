f1b8a163f3fc15e03ef20af636cfb6053044f841
4m9r <amirali.safizadeh@gmail.com>
2020-12-29 18:00:14 +0100

    initial commit-bug free

---
+++ connect4/Main.java
@@ -0,0 +1,15 @@
+package com.ai.project;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new GUI();
+            }
+        });
+    }
+}
