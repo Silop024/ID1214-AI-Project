49937e4cab1341ed8733261254623cfd939433d9
4m9r <amirali.safizadeh@gmail.com>
2020-12-29 20:14:28 +0100

    a working winning method

--- connect4/Game.java
+++ connect4/Game.java
@@ -8,54 +8,37 @@ public class Game {
 
     // finding the winner
     public boolean winner(Board board,String playerColor){
-        boolean win = false;
-        int winStreak = 0;
 
-        // vertical matches
-        for (int row = 0; row < NUMBER_OF_ROW ; row++) {
-            winStreak = 0;
+        for (int row = 0; row < NUMBER_OF_ROW ; row++)
+            for (int column = 0; column < NUMBER_OF_COLUMN - 3; column++) {
+                if (board.getPiece(row, column) != null &&
+                        board.getPiece(row , column + 1) != null &&
+                        board.getPiece(row , column + 2) != null &&
+                        board.getPiece(row , column + 3) != null )
+
+                    if (board.getPiece(row, column).getColor().equals(playerColor) &&
+                            board.getPiece(row, column + 1).getColor().equals(playerColor) &&
+                            board.getPiece(row, column + 2).getColor().equals(playerColor) &&
+                            board.getPiece(row, column + 3).getColor().equals(playerColor)
+                    ) return true;
+            }
+
+
+
+        for (int row = 0; row < NUMBER_OF_ROW - 3 ; row++)
             for (int column = 0; column < NUMBER_OF_COLUMN; column++) {
-                if (board.getPiece(row, column) != null)
-                    if (board.getPiece(row, column).getColor().equals(playerColor))
-                        winStreak++;
+                if (board.getPiece(row, column) != null &&
+                        board.getPiece(row + 1, column) != null &&
+                        board.getPiece(row + 2, column) != null &&
+                        board.getPiece(row + 3, column) != null )
 
-                    else {
-                        if (winStreak == 4)
-                            break;
-                        else
-                            winStreak = 0;
-                    }
+                    if (board.getPiece(row, column).getColor().equals(playerColor) &&
+                            board.getPiece(row + 1, column).getColor().equals(playerColor) &&
+                            board.getPiece(row + 2, column).getColor().equals(playerColor) &&
+                            board.getPiece(row + 3, column).getColor().equals(playerColor)
+                    )
+                        return true;
             }
-            if (winStreak == 4)
-                break;
-        }
-
-        if (winStreak == 4)
-            win = true;
-
-
-        // horizontal matches
-        for (int column = 0; column < NUMBER_OF_COLUMN; column++) {
-            winStreak = 0;
-            for (int row = 0; row < NUMBER_OF_ROW; row++) {
-                if (board.getPiece(row, column) != null)
-                    if (board.getPiece(row, column).getColor().equals(playerColor))
-                        winStreak++;
-                    else {
-                        if (winStreak == 4)
-                            break;
-
-                        else
-                            winStreak = 0;
-                    }
-            }
-            if (winStreak == 4)
-                break;
-        }
-        if (winStreak == 4)
-            win = true;
-
-
 
 
         // diagonal matches
@@ -70,10 +53,8 @@ public class Game {
                             board.getPiece(row - 1, column + 1).getColor().equals(playerColor) &&
                             board.getPiece(row - 2, column + 2).getColor().equals(playerColor) &&
                             board.getPiece(row - 3, column + 3).getColor().equals(playerColor)
-                    ) {
-                        win = true;
-                        break;
-                    }
+                    )
+                        return true;
             }
 
         for (int row = 0; row < NUMBER_OF_ROW - 3; row++)
@@ -87,10 +68,8 @@ public class Game {
                             board.getPiece(row + 1, column + 1).getColor().equals(playerColor) &&
                             board.getPiece(row + 2, column + 2).getColor().equals(playerColor) &&
                             board.getPiece(row + 3, column + 3).getColor().equals(playerColor)
-                    ) {
-                        win = true;
-                        break;
-                    }
+                    )
+                        return true;
             }
 
 
@@ -99,6 +78,6 @@ public class Game {
 
 
 
-        return win;
+        return false;
     }
 }
